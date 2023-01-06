b9f651df2349ec90afe2e93f3b4a688848021155
Robert Benavides <robertbenavidesgomez@gmail.com>
2023-01-06 15:21:22 +0100

    Deleting in Array

---
+++ Deleting_in_Array.cpp
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Array
+{
+	int A[10];
+	int size;
+	int length;
+};
+void Display(struct Array* arr)
+{
+	int i;
+	printf("\nElements are\n");
+	for (i = 0; i < (*arr).length; i++)
+	{
+		printf("%d ", arr->A[i]);
+	}
+}
+void Append(struct Array* arr, int x)
+{
+	if (arr->length < arr->size)
+	{
+		arr->A[arr->length++] = x;
+	}
+}
+void Insert(struct Array* arr, int index, int x)
+{
+	int i;
+	if (index >= 0 && index <= arr->length)
+	{
+		for (i = arr->length; i > index; i--)
+		{
+			arr->A[i] = arr->A[i - 1];
+
+		}
+		arr->A[index] = x;
+		arr->length++;
+	}
+}
+int Delete(struct Array* arr, int index)
+{
+	int i;
+	int x = 0;
+	if (index >= 0 && index < arr->length)
+	{
+		x = arr->A[index];
+		for (i = index; i < arr->length - 1; i++)
+		{
+			arr->A[i] = arr->A[i + 1];
+		}
+		arr->length--;
+		return x;
+	}
+	return 0;
+}
+int main()
+{
+
+	struct Array arr = { {2,3,4,5,6},10,5 };
+
+	printf("%d\n", Delete(&arr, 4));
+	Display(&arr);
+
+
+	return 0;
+}
