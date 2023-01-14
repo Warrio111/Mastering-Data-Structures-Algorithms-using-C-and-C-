a40ff67015040408c9160c69017308047a363609
Robert Benavides <robertbenavidesgomez@gmail.com>
2023-01-15 00:13:50 +0100

    Linked List in C

--- Linked List.cpp
+++ Linked List.cpp
@@ -4,7 +4,7 @@ struct Node
 {
 	int data;
 	struct Node* next;
-}*first = NULL;
+}*first, * last = NULL, * second = NULL, * third = NULL;
 void create(int A[], int n)
 {
 	int i;
@@ -13,6 +13,24 @@ void create(int A[], int n)
 	first->data = A[0];
 	first->next = NULL;
 	last = first;
+
+	for (i = 1; i < n; i++)
+	{
+		t = (struct Node*)malloc(sizeof(struct Node));
+		t->data = A[i];
+		t->next = NULL;
+		last->next = t;
+		last = t;
+	}
+}
+void create2(int A[], int n)
+{
+	int i;
+	struct Node* t, * last;
+	second = (struct Node*)malloc(sizeof(struct Node));
+	second->data = A[0];
+	second->next = NULL;
+	last = second;
 
 	for (i = 1; i < n; i++)
 	{
@@ -165,14 +183,235 @@ void Insert(struct Node* p, int index, int x)
 	}
 
 }
+void InsertLast(int x)
+{
+	struct Node* t;
+	t = (struct Node*)malloc(sizeof(struct Node));
+	t->data = x;
+	if (NULL == first)
+	{
+		first = last = t;
+	}
+	else {
+		last->next = t;
+		last = t;
+	}
+}
+void InsertSorted(struct Node* p, int x)
+{
+	struct Node* q = NULL;
+	struct Node* t = NULL;
+	t = (struct Node*)malloc(sizeof(struct Node));
+	t->data = x;
+	t->next = NULL;
+	if (NULL == first)
+	{
+		first = t;
+	}
+	else
+	{
+		while ((p) && (p->data < x))
+		{
+			q = p;
+			p = p->next;
+		}
+		if (p == first)
+		{
+			t->next = first;
+			first = t;
+		}
+		else
+		{
+			t->next = q->next;
+			q->next = t;
+		}
+	}
+
+
+
+}
+int DeletingNode(int index)
+{
+	struct Node* p;
+	struct Node* q = NULL;
+	int i;
+	int x;
+	x = -1;
+	if (index == 1)
+	{
+		x = first->data;
+		p = first;
+		first = first->next;
+		free(p);
+	}
+	else
+	{
+		p = first;
+		q = NULL;
+		for (i = 0; i < index - 1; i++)
+		{
+			q = p;
+			p = p->next;
+		}
+		if (p)
+		{
+			q->next = p->next;
+			x = p->data;
+			free(p);
+		}
+	}
+	return x;
+}
+int isSorted(struct Node* p)
+{
+	int x = INT_MIN;
+	while (NULL != p)
+	{
+		if (p->data < x)
+		{
+			return 0;
+		}
+		x = p->data;
+		p = p->next;
+	}
+	return 1;
+}
+void RemoveDuplicate(struct Node* p)
+{
+	struct Node* q = p->next;
+	while (NULL != q)
+	{
+		if (p->data != q->data)
+		{
+			p = q;
+			q = q->next;
+		}
+		else
+		{
+			p->next = q->next;
+			free(q);
+			q = p->next;
+		}
+	}
+}
+void ReverseWithArray(struct Node* p)
+{
+	int* A;
+	int i;
+	i = 0;
+	struct Node* q = p;
+	A = (int*)malloc(sizeof(int) * count(p));
+	while (NULL != q)
+	{
+		A[i] = q->data;
+		q = q->next;
+		i++;
+	}
+	q = p;
+	i--;
+	while (NULL != q)
+	{
+		q->data = A[i];
+		q = q->next;
+		i--;
+	}
+
+}
+void ReverseWithSlidiningPointer(struct Node* p)
+{
+	struct Node* q = NULL, * r = NULL;
+	while (NULL != p)
+	{
+		r = q;
+		q = p;
+		p = p->next;
+		q->next = r;
+	}
+	first = q;
+}
+void ReverseWithRecursivity(struct Node* q, struct Node* p)
+{
+	if (p)
+	{
+		ReverseWithRecursivity(p, p->next);
+		p->next = q;
+	}
+	else
+	{
+		first = q;
+	}
+}
+void Concat(struct Node* p, struct Node* q)
+{
+	third = p;
+	while (NULL != p->next)
+	{
+		p = p->next;
+	}
+	p->next = q;
+}
+void Merge(struct Node* p, struct Node* q)
+{
+	struct Node* last;
+	if (p->data < q->data)
+	{
+		third = last = p;
+		p = p->next;
+		third->next = NULL;
+	}
+	else
+	{
+		third = last = q;
+		q = q->next;
+		third->next = NULL;
+	}
+	while (p && q)
+	{
+		if (p->data < q->data)
+		{
+			last->next = p;
+			last = p;
+			p = p->next;
+			last->next = NULL;
+		}
+		else
+		{
+			last->next = q;
+			last = q;
+			q = q->next;
+			last->next = NULL;
+		}
+	}
+	if (p) { last->next = p; }
+	if (q) { last->next = q; }
+}
+int isLoop(struct Node* f)
+{
+	struct Node* p, * q;
+	p = q = f;
+	do
+	{
+		p = p->next;
+		q = q->next;
+		q = q ? q->next : q;
+	} while (p && q && p != q);
+	if (p == q)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
 int main()
 {
-	Insert(first, 0, 12);
-	Insert(first, 1, 20);
-	Insert(first, 2, 30);
-	Insert(first, 2, 40);
-
-	Display(first);
-
+	struct Node* t1, * t2;
+	int A[] = { 10,20,30,40,50 };
+	create(A, 5);
+	t1 = first->next->next;
+	t2 = first->next->next->next->next;
+	t2->next = t1;
+	printf("\n%d\n", isLoop(first));
 	return 0;
 }
