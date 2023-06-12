a7c41c535400d7f3057538531d6dff03bb86ba68
Robert Benavides <robertbenavidesgomez@gmail.com>
2023-06-12 15:38:31 +0200

    Recursive Insert and Delete

--- Binary Search Tree.c
+++ Binary Search Tree.c
@@ -88,14 +88,110 @@ struct Node* Search(int key)
 	return NULL;
 }
 
+struct Node* RInsert(struct  Node* p, int key)
+{
+	struct Node* t = NULL;
+
+	if (NULL == p)
+	{
+		t = (struct Node*)malloc(sizeof(struct Node));
+		t->data = key;
+		t->lchild = t->rchild = NULL;
+		return t;
+	}
+	if (key < p->data)
+	{
+		p->lchild = RInsert(p->lchild, key);
+	}
+	else if (key > p->data)
+	{
+		p->rchild = RInsert(p->rchild, key);
+	}
+	return p;
+}
+
+int Height(struct Node* p)
+{
+	int x, y;
+	if (NULL == p)
+	{
+		return 0;
+	}
+	x = Height(p->lchild);
+	y = Height(p->rchild);
+	return x > y ? x + 1 : y + 1;
+}
+
+struct Node* InPre(struct Node* p)
+{
+	while (p && p->rchild != NULL)
+	{
+		p = p->rchild;
+	}
+	return p;
+}
+
+struct Node* InSucc(struct Node* p)
+{
+	while (p && p->lchild != NULL)
+	{
+		p = p->lchild;
+	}
+	return p;
+}
+struct Node* Delete(struct Node* p, int key)
+{
+	struct Node* q;
+	if (NULL == p)
+	{
+		return NULL;
+	}
+	if (NULL == p->lchild && NULL == p->rchild)
+	{
+		if (p == root)
+		{
+			root = NULL;
+		}
+		free(p);
+		return NULL;
+	}
+
+	if (key < p->data)
+	{
+		p->lchild = Delete(p->lchild, key);
+	}
+	else if (key > p->data)
+	{
+		p->rchild = Delete(p->rchild, key);
+	}
+	else
+	{
+		if (Height(p->lchild) > Height(p->rchild))
+		{
+			q = InPre(p->lchild);
+			p->data = q->data;
+			p->lchild = Delete(p->lchild, q->data);
+		}
+		else
+		{
+			q = InSucc(p->rchild);
+			p->data = q->data;
+			p->rchild = Delete(p->rchild, q->data);
+		}
+	}
+	return p;
+}
+
 int main(int argc, char* argv[])
 {
 	struct Node* temp;
-	Insert(10);
-	Insert(5);
-	Insert(20);
-	Insert(8);
-	Insert(30);
+	root = RInsert(root, 10);
+	RInsert(root, 5);
+	RInsert(root, 20);
+	RInsert(root, 8);
+	RInsert(root, 30);
+
+	Delete(root, 20);
 
 	Inorder(root);
 	printf("\n");
